546c67449c9dd00b99dcdbaf596c2ec084e069ab
Jon Grover <jon@Durandal.local>
2010-01-27 22:25:20 -0800

    Made minor initial changes to scotch model

--- models/scotch.py
+++ models/scotch.py
@@ -1,8 +1,8 @@
 from google.appengine.ext import db
 
 class Scotch(db.Model):
-    name = db.StringProperty(required=True)
-    age = db.IntegerProperty(required=True)
-    lastTastedOn = db.DateProperty()
-    owner = db.UserProperty(required=True)
-	type = db.StringProperty(choices=set(["Single", "malt", "blended"]))
+  name = db.StringProperty(required=True)
+  age = db.IntegerProperty(required=True)
+  lastTastedOn = db.DateProperty()
+  owner = db.UserProperty()
+  scotchType = db.StringProperty(choices=set(["Single", "malt", "blended"]))
