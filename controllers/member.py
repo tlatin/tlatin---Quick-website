312ef7c3b3ad47f4cbeead6c8842e3c628d21d1e
Jon Grover <jon@Durandal.local>
2010-01-27 23:40:58 -0800

    Changes made to the new member view, I think. We've been drinking.

---
+++ controllers/member.py
@@ -0,0 +1,31 @@
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+
+class MemberController(webapp.RequestHandler):
+
+  # GET
+  def get(self, url_data):    
+      if url_data:
+          if '/new' == url_data:
+              self.new({})
+          elif '/search' == url_data:
+              params = self.parameterize() 
+              self.search(params)
+          elif '/edit' == url_data[-5:]:
+              event = Event.get_by_id(int(url_data[1:-5]))
+              self.edit(event)
+          else:
+              self.show(url_data[1:])
+      else:
+          self.list()
+          
+  def new(self, params):
+    template_values = {
+
+      }
+    
+    path = os.path.join(os.path.dirname(__file__), '..', 'views', 'member_new.html')
+    self.response.out.write(template.render(path, template_values))
