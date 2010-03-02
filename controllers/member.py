02047e8acd57e48908bbc02404f7df75b6f37f1e
Jon Grover <jon@durandal.(none)>
2010-03-01 21:40:42 -0800

    Adding member creation form

--- controllers/member.py
+++ controllers/member.py
@@ -2,11 +2,16 @@ import os
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+from models.member import MemberForm
+from models.member import Member
+from google.appengine.ext.webapp.util import login_required
+from google.appengine.api import users
 
 
 class MemberController(webapp.RequestHandler):
 
   # GET
+  @login_required
   def get(self, url_data):    
       if url_data:
           if '/new' == url_data:
@@ -24,8 +29,41 @@ class MemberController(webapp.RequestHandler):
           
   def new(self, params):
     template_values = {
-
+      'member_form' : MemberForm(),
+      'user' : users.get_current_user()
     }
     
     path = os.path.join(os.path.dirname(__file__), '..', 'views', 'member_new.html')
     self.response.out.write(template.render(path, template_values))
+    
+  def post(self, url_data):
+    if url_data:
+      if '/new' == url_data:
+        self.create_member({})
+      elif '/search' == url_data:
+        params = self.parameterize() 
+        self.search(params)
+      elif '/edit' == url_data[-5:]:
+        event = Event.get_by_id(int(url_data[1:-5]))
+        self.edit(event)
+      else:
+        self.show(url_data[1:])
+    else:
+      self.list()
+        
+  def create_member(self, params):
+    data = MemberForm(data=self.request.POST)
+    if data.is_valid():
+      # Save the data, and redirect to the view page
+      member = data.save(commit=False)
+      member.user = users.get_current_user()
+      member.put()
+      self.redirect('/')
+    else:
+      # Reprint the form
+      template_values = {
+        'member_form' : data
+      }
+
+      path = os.path.join(os.path.dirname(__file__), '..', 'views', 'member_new.html')
+      self.response.out.write(template.render(path, template_values))
--- models/member.py
+++ models/member.py
@@ -1,8 +1,18 @@
 from google.appengine.ext import db
 
+from google.appengine.ext.webapp import template
+from google.appengine.ext.db import djangoforms
+
 class Member(db.Model):
   name = db.StringProperty(required=True)
   title = db.StringProperty()
-  user = db.UserProperty(required=True) 
+  user = db.UserProperty(required=False) 
   # scotches = reference from scotches.py
   # comments = reference from comments.py
+  
+  
+  
+class MemberForm(djangoforms.ModelForm):
+      class Meta:
+          model = Member
+          exclude = ['user']
