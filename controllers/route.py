5b97b92e722b3941422adc4c9515f77800b5d3ee
Toby Latin-Stoermer <tlatin@gmail.com>
2011-11-17 22:18:25 -0800

    Removing comment controller references

--- controllers/route.py
+++ controllers/route.py
@@ -8,12 +8,12 @@ from google.appengine.ext.webapp import template
 from controllers.index import IndexController
 from controllers.member import MemberController
 from controllers.scotch import ScotchController
-from controllers.comment import CommentController
+#from controllers.comment import CommentController
 
 def main():
   application = webapp.WSGIApplication(
        [('/', IndexController),
-        ('/scotch/(\d)/comments(|/\d+|/new|/\d+/edit)', CommentController),
+        #('/scotch/(\d)/comments(|/\d+|/new|/\d+/edit)', CommentController),
         ('/members(|/\d+|/new|/search|/\d+/edit)', MemberController),
         ('/scotch(|/\d+|/new|/\d+/edit)', ScotchController),
        ],
